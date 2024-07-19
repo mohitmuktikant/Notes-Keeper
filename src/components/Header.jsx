e3cd7883f4739173ca3954b03cbe1f6a245eb343
Mohit Muktikant <101824646+mohitmuktikant@users.noreply.github.com>
2024-07-19 21:19:38 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@mui/icons-material/Checklist";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Keep Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
